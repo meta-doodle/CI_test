e5a1aa21ecdb672f66a5cf96b70a2edd322d40a1
Brandon <yberion.ins@gmail.com>
2020-02-13 00:43:59 +0100

    Java 11 feature test

--- src/main/java/fr/helloyou/Main.java
+++ src/main/java/fr/helloyou/Main.java
@@ -10,6 +10,11 @@ public class Main
     public static void main(String[] args)
     {
         Personne thomas = new PersonneImpl("Thomas");
+        
+        if (thomas.getName().isBlank())
+        {
+            Logger.info("Java 11 feature test");
+        }
 
         Logger.info("Hello " + thomas.getName() + "!");
     }
